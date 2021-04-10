66e7d87d9aee28cbfb09fae5ee3f05a5d92a829e
cc <478959472@qq.com>
2021-04-10 13:15:53 +0800

    使用ASCII码值记录字符

--- src/main/java/learning/Test.java
+++ src/main/java/learning/Test.java
@@ -10,50 +10,18 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Test {
-    /**
-     * 1. 找到极值点（极值点也是用二分法找到的）(extreme point, binary method)
-     * 2. 根据极值点将数组分为两部分，分别进行二分搜索，查找key值
-     * according to the extreme point, divide the array into two parts
-     * use Binary search respectively find the key value
-     * @param a
-     * @param key
-     * @return
-     */
-    public static int find2(int[] a, int key){
-        int lo = 0;
-        int hi = a.length-1;
-        int mid = -1;
-        while(lo <= hi){
-            mid = (lo + hi) / 2;
-            if(a[mid] > a[mid - 1]){
-                lo = mid + 1;
-            }else{
-                hi = mid - 1;
-            }
-        }
-        System.out.println("lo:"+lo +" hi:"+hi+" mid:"+mid);
-        int leftArray[] = Arrays.copyOfRange(a, 0, hi);
-        int rightArray[] = Arrays.copyOfRange(a, lo - 1, a.length - 1);
-        int left = BinarySearch.rank(key, leftArray);
-        rightArray= reverse3(rightArray);
-        int right = BinarySearch.rank(key, rightArray);
 
-        return left > right ? left : right;
+    public enum PizzaStatus {
+        ORDERED,
+        READY,
+        DELIVERED;
     }
 
     public static void main(String[] args) {
-        int[] a = { 2,3, 4,5,6,10,13,15,14,12,11,9};
-
-        System.out.println( find2(a, 12) );
-
-
+        System.out.println(PizzaStatus.ORDERED.name());//ORDERED
+        System.out.println(PizzaStatus.ORDERED);//ORDERED
+        System.out.println(PizzaStatus.ORDERED.name().getClass());//class java.lang.String
+        System.out.println(PizzaStatus.ORDERED.getClass());//class shuang.kou.enumdemo.enumtest.PizzaStatus
     }
 
-    public static int[] reverse3(int[] nums) {
-        int[] reversed = new int[nums.length];
-        for (int i=0; i<nums.length; i++) {
-            reversed[i] = nums[nums.length - 1 - i];
-        }
-        return reversed;
-    }
 }
---
+++ src/main/java/learning/leetcode/Solution.java
@@ -0,0 +1,33 @@
+package learning.leetcode;
+
+public class Solution {
+    /**
+     * 思路： 1、用一个长度256的int数组标识所有ACCII码字符，用来储存每个字符出现的次数
+     * 2、遍历字符串s1，字符每出现一次，该字符对应的数组位置加一。 3、遍历完s1后，遍历字符串s2，字符每出现一次，该字符对应的数组位置减一。
+     * 4、如果某个字符次数不够减，则返回false，否则直至遍历结束返回true
+     */
+    public static boolean checkPermutation(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int[] temp = new int[256];
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            temp[c]++;
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            char c = s2.charAt(i);
+            if (temp[c] == 0) {
+                return false;
+            } else {
+                temp[c]--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPermutation("qqppmmzz", "mmppqqzz"));
+    }
+
+}
