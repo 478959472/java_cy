8c1bb7f93538d32f1086c3a6cc05c0a05effa814
cc <478959472@qq.com>
2021-06-10 11:34:28 +0800

    netty实现http请求demo

--- src/main/java/learning/leetcode/Solution.java
+++ src/main/java/learning/leetcode/Solution.java
@@ -1,6 +1,21 @@
 package learning.leetcode;
 
+import java.util.HashSet;
+
 public class Solution {
+
+    public boolean isUnique(String astr) {
+        if(astr.length() > 1){
+            HashSet<Character> set = new HashSet<>(astr.length());
+            for(char c : astr.toCharArray()){
+                if(!set.add(c)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     /**
      * 思路： 1、用一个长度256的int数组标识所有ACCII码字符，用来储存每个字符出现的次数
      * 2、遍历字符串s1，字符每出现一次，该字符对应的数组位置加一。 3、遍历完s1后，遍历字符串s2，字符每出现一次，该字符对应的数组位置减一。
